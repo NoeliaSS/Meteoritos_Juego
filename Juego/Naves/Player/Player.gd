673f3c9bad445bf69f6ecbddf3c545773f15bbee
NoeliaSS <noelia.ss86@gmail.com>
2022-07-25 20:33:44 -0300

    Se agrega Spawn y efecto

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends RigidBody2D
 
+##Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
 
 ##Atributos Export
 export var potencia_motor:int = 20
@@ -9,6 +11,7 @@ export var estela_maxima:int = 150
 
 
 ## Atributos
+var estado_actual:int = ESTADO.SPAWN
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
@@ -17,9 +20,12 @@ onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
 ## Métodos
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
 	#Disparo rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
@@ -45,9 +51,37 @@ func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 
 func _process(_delta: float) -> void:
 	player_input()
+	
+func _ready() -> void:
+	controlador_estados(estado_actual)
+	
 
 # Métodos Custom
+
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			Eventos.emit_signal("nave_destruida", global_position, 3)
+			queue_free()
+		_:
+			printerr("Error de estado")
+		
+	estado_actual = nuevo_estado
+	
 func player_input() -> void:
+	if not esta_input_activo():
+		return
+		
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
@@ -69,3 +103,16 @@ func player_input() -> void:
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
 		
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	
+	return true
+
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
+
+## Señales internas
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
