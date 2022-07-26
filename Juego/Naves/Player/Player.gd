d7e754256aace53751be351e138c8b2a4c67d87d
NoeliaSS <noelia.ss86@gmail.com>
2022-07-26 12:17:56 -0300

    Escudo y absorción de disparos

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,16 +1,14 @@
 class_name Player
 extends RigidBody2D
 
-##Enums
+## Enums
 enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
 
-##Atributos Export
+## Atributos Export
 export var potencia_motor:int = 20
-export var potencia_rotacion:int =280
+export var potencia_rotacion:int = 280
 export var estela_maxima:int = 150
 export var hitpoints:float = 15.0
-
-
 
 ## Atributos
 var estado_actual:int = ESTADO.SPAWN
@@ -24,17 +22,26 @@ onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx:AudioStreamPlayer = $ImpactoSFX
+onready var escudo:Escudo = $Escudo
 
-## Métodos
+## Metodos
+func _ready() -> void:
+	controlador_estados(estado_actual)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if not esta_input_activo():
 		return
-	#Disparo rayo
+	
+	# Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
-		
+	
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
+	
+	# Control Escudo
+	if event.is_action_pressed("escudo") and not escudo.get_esta_activado():
+		escudo.activar()
 	
 	# Control estela y sonido motor
 	if event.is_action_pressed("mover_adelante"):
@@ -47,20 +54,15 @@ func _unhandled_input(event: InputEvent) -> void:
 	if (event.is_action_released("mover_adelante")
 		or event.is_action_released("mover_atras")):
 			motor_sfx.sonido_off()
-		
-func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	player_input()
-	
-func _ready() -> void:
-	controlador_estados(estado_actual)
-	
 
-# Métodos Custom
-
+## Metodos Custom
 func controlador_estados(nuevo_estado: int) -> void:
 	match nuevo_estado:
 		ESTADO.SPAWN:
@@ -80,45 +82,44 @@ func controlador_estados(nuevo_estado: int) -> void:
 			printerr("Error de estado")
 		
 	estado_actual = nuevo_estado
-	
+
 func player_input() -> void:
 	if not esta_input_activo():
 		return
-		
+	
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
 		empuje = Vector2(potencia_motor, 0)
 	elif Input.is_action_pressed("mover_atras"):
 		empuje = Vector2(-potencia_motor, 0)
-		
+	
 	# Rotacion
 	dir_rotacion = 0
 	if Input.is_action_pressed("rotar_antihorario"):
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
-
-#Disparo
+	
+	# Disparo
 	if Input.is_action_pressed("disparo_principal"):
 		canion.set_esta_disparando(true)
 		
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
-		
+
 func esta_input_activo() -> bool:
 	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
 		return false
 	
 	return true
-	
+
 func recibir_danio(danio: float) -> void:
 	hitpoints -= danio
 	if hitpoints <= 0.0:
 		destruir()
 	
 	impacto_sfx.play()
-
 
 func destruir() -> void:
 	controlador_estados(ESTADO.MUERTO)
